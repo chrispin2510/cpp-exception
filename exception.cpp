9bd2529f5b88f4fe0e9da6eb9787c73ff47ed639
Chrispin Charles <chrispin.charles@outlook.com>
2021-02-13 14:07:31 +0530

    Added a line which manually throws an exception

--- exception.cpp
+++ exception.cpp
@@ -6,6 +6,8 @@ using std::endl;
 /* function with potential division by 0 exception */
 int abc (int a, int b, int c)
 {
+    if (a <= 0 || b <= 0 || c <= 0)
+        throw "All parameters should be > 0";
     return a + b * c + b / c;
 }
 
