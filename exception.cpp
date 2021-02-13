0f9a546603fc086422279305f304af75e08c083d
Chrispin Charles <chrispin.charles@outlook.com>
2021-02-13 14:28:50 +0530

    Modified to add a generic way for handling exception

--- exception.cpp
+++ exception.cpp
@@ -2,20 +2,19 @@
 
 using std::cout;
 using std::endl;
+using std::exception;
 
 /* function with potential division by 0 exception */
 int abc (int a, int b, int c)
 {
-    if (a <= 0 || b <= 0 || c <= 0)
-        throw "All parameters should be > 0";
     return a + b * c + b / c;
 }
 
 int main()
 {
-    try { cout << abc(2,1,0); }
-    catch (const char*& e) {
-        cout << "an exception hash been thrown: " << e << endl;
+    try { cout << abc(2,1,0) << endl; }
+    catch (const exception& e) {
+        cout << "Caught " << e.what() << endl;
     }
     return 0;
 }
